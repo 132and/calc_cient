eb43b2efc558acd177ebb0295376dfcbc6367957
132and <53800696+132and@users.noreply.github.com>
2019-11-05 14:14:13 -0300

    subi errado correção

--- application.py
+++ application.py
@@ -4,5 +4,8 @@ application = Flask(__name__)
 
 @application.route('/inicio')
 def inicio():
-    return render_template('main.html')
+    return render_template('inicio.html')
+
+if __name__ == '__main__':
+    application.run(debug=ON, host = '127.0.0.1')
     
